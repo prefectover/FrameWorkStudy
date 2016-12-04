7aac75f971c1cb273620a1bedce498ac74998af9
liangxiegame <shanksking@hotmail.com>
2016-12-04 15:32:30 +0800

    组织架构调整

---
+++ Assets/_Script/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using QFramework;
+
+public class GameManager : QSingleton<GameManager> {
+
+	public IEnumerator Launch() {
+
+		yield return null;
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
