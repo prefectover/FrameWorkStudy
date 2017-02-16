1dd1883bf103cf0682c77e7258aeec36183649b7
liangxiegame <liangxiegame@163.com>
2017-02-16 23:29:06 +0800

    提交QFramework0.0.5

---
+++ Assets/Example/3.FSMPerformance/Main.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using QFramework;
+using QFramework.UI;
+
+namespace QFramework.UIFramework.Example3 {
+
+	public class Main : MonoBehaviour {
+
+		void Awake() {
+			QUIManager.Instance.OpenUI<UIUnityMsgRootPage> (QUILevel.Common, null, null);
+		}
+	}
+}
---
+++ Assets/Example/3.FSMPerformance/MsgDefine.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using QFramework;
+using QFramework.UI;
+using QFramework.Event;
+
+namespace QFramework.UIFramework.Example3 {
+
+	public enum UIEvent {
+		Began = QMgrID.UI,
+		Hello,
+		Ended
+	}
+}
